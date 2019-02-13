9fd9c35c4d098a2f512d45a3c0c851c3f243ac9a
Robert Glaser <robertbglaser@gmail.com>
2019-02-13 12:41:02 -0800

    added listeners and the reset button works now.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,43 @@ class App extends Component {
     this.state={
       suggestedDelay:100,
       numOfChairs:100,
-      
+      baseDelay:100,
+      delayAmt:100,
+      baseChairs:100
     }
   }
+
+  handleChairs =(event) =>{
+    
+    this.setState({numOfChairs: event.target.value})
+    console.log("in handleChairs " , this.state.numOfChairs)
+   
+    };
+
+    runSimulation =(event) =>{
+      console.log("in runSimulation")
+     
+      };
+
+    handleDelay =(event) =>{
+      this.setState({delayAmt: event.target.value})
+      console.log("in handledelay " , this.state.delayAmt)
+      };
+
+  
+    handleSingleStep =(event) =>{
+      console.log("in singlestep")
+      };
+
+      handleReset =(event) =>{
+        this.setState({delayAmt: this.state.baseDelay} )
+        this.setState({numOfChairs: this.state.baseChairs} )
+        console.log("in reset")
+        };
+  
+  
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -24,19 +57,19 @@ class App extends Component {
               </div> 
 
             <div>
-              <input type="text" onChange="handleChairs()" id="chairCount" placeholder="100" value="100"/>
+              <input type="text" onChange={this.handleChairs} id="chairCount" value={this.state.numOfChairs}/>
               <label className="labelCopy">Chairs</label>
             </div>
    
       
       <div >
-        <button className="runBtn"onClick="runSimulation()">Run simulation </button>
-        <input type="text"  id="delay" onChange="handleDelay()"placeholder="100" value="100"/>
+        <button className="runBtn"onClick={this.runSimulation}>Run simulation </button>
+        <input type="text"  id="delay" onChange={this.handleDelay} value={this.state.delayAmt}/>
           <label className="labelCopy">Ms</label>
       </div>
       <div>
-            <button  onClick="singleStep()">Single Step</button>
-            <button className="removeBtn" onClick="reset()">Reset</button>
+            <button  onClick={this.handleSingleStep}>Single Step</button>
+            <button className="removeBtn" onClick={this.handleReset}>Reset</button>
       </div>
 
           
